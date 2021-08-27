f7ccffd52c97f6561608012edbb8eef1805a75e1
Joshua Horner <lesliewbusiness@gmail.com>
2021-08-26 18:50:31 -0600

    Create Game class implementation file

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,15 @@
-const data = require('./data');
+const data = require('../src/data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +19,15 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
-}
+
+  start() {
+    const cards = [];
+    prototypeQuestions.forEach(card => cards.push(new Card(card.id, card.question, card.answers, card.correctAnswer)));
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+};
 
 module.exports = Game;
